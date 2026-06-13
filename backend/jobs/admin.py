26693d474944a028d903899f1898d09647005edf
Risriddle <risriddle62442@gmail.com>
2026-06-13 16:43:40 +0530

    added db model

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Job, JobStatus, JobCategory
+
+admin.site.register(Job)
+admin.site.register(JobStatus)
+admin.site.register(JobCategory)
--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class JobStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class JobCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+
+    profile_picture = models.ImageField(
+        upload_to="job_profiles/",
+        blank=True,
+        null=True
+    )
+
+    description = models.TextField()
+
+    statuses = models.ManyToManyField(
+        JobStatus,
+        related_name="jobs"
+    )
+
+    categories = models.ManyToManyField(
+        JobCategory,
+        related_name="jobs"
+    )
+
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100,db_index=True)
+    state = models.CharField(max_length=100,db_index=True)
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.end_date < self.start_date:
+            raise ValidationError(
+                "End date cannot be before start date."
+            )
+
+    def __str__(self):
+        return self.title
