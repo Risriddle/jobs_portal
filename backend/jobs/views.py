a1e06270784593aa09a3b07270a945bab9a4d923
Risriddle <risriddle62442@gmail.com>
2026-06-13 17:23:02 +0530

    added duplicate job view

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -8,6 +8,9 @@ from .serializers import (
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all().order_by("-created_at")
@@ -21,6 +24,31 @@ class JobViewSet(viewsets.ModelViewSet):
     search_fields = ["title"]
 
     filterset_fields = ["statuses"]
+
+
+
+    @action(detail=True, methods=["post"])
+    def duplicate(self, request, pk=None):
+
+        job = self.get_object()
+
+        duplicated_job = Job.objects.create(
+            title=job.title,
+            profile_picture=job.profile_picture,
+            description=job.description,
+            address=job.address,
+            city=job.city,
+            state=job.state,
+            start_date=job.start_date,
+            end_date=job.end_date,
+        )
+
+        duplicated_job.statuses.set(job.statuses.all())
+        duplicated_job.categories.set(job.categories.all())
+
+        serializer = self.get_serializer(duplicated_job)
+
+        return Response(serializer.data)
 
 
 
